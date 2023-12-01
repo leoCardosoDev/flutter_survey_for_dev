5ce1776bca7fb89f32768fb801e10925bfae04a8
leoCardosoDev <leocardosodev@gmail.com>
2023-12-01 08:16:17 -0300

    feat: ensure HttpAdapter throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -76,6 +76,18 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test("Should return BadRequestError if post returns 400", () async {
+      mockResponse(400);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test("Should return UnauthorizedError if post returns 401", () async {
+      mockResponse(401);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test("Should return ServerError if post returns 500", () async {
       mockResponse(500);
       final future = sut.request(url: url, method: 'post');
