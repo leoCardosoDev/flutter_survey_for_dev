a9c2c68201d651c44b5b56175e296bfb88ae846c
leoCardosoDev <leocardosodev@gmail.com>
2023-12-01 08:21:25 -0300

    feat: ensure HttpAdapter throws ForbbidenError on 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -88,6 +88,12 @@ void main() {
       expect(future, throwsA(HttpError.unauthorized));
     });
 
+    test("Should return ForbbidenError if post returns 403", () async {
+      mockResponse(403);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.forbbiden));
+    });
+
     test("Should return ServerError if post returns 500", () async {
       mockResponse(500);
       final future = sut.request(url: url, method: 'post');
