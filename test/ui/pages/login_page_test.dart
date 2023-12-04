64e6502b3023b59eba9d5f2da93c78cc4b7a8c03
leoCardosoDev <leocardosodev@gmail.com>
2023-12-04 17:00:41 -0300

    test: add test inital state for LoginPage with field E-mail

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:survey_for_dev/ui/pages/pages.dart';
+
+void main() {
+  testWidgets('Should load with correct initial state', (WidgetTester tester) async {
+    const loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('E-mail'), matching: find.byType(Text));
+    expect(emailTextChildren, findsOneWidget);
+  });
+}
