edf3438f6c4f361996def97979beeded286a367f
leoCardosoDev <leocardosodev@gmail.com>
2023-12-06 14:38:29 -0300

    test: ensure LoginPage calls LoginPresenter with validateEmail

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,10 +1,18 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
 import 'package:survey_for_dev/ui/pages/pages.dart';
 
+class LoginPresenterSpy extends Mock implements LoginPresenter {}
+
 void main() {
+  late LoginPresenter presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
-    const loginPage = MaterialApp(home: LoginPage());
+    presenter = LoginPresenterSpy();
+    final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
   testWidgets('Should load with correct initial state', (WidgetTester tester) async {
@@ -17,5 +25,12 @@ void main() {
 
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, null);
+  });
+
+   testWidgets('Should call validate with correct email', (WidgetTester tester) async {
+    await loadPage(tester);
+    final email = faker.internet.email();
+    await tester.enterText(find.bySemanticsLabel('E-mail'), email);
+    verify(() => presenter.validateEmail(email));
   });
 }
