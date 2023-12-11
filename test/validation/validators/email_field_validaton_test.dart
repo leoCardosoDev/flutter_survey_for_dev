6a1aa402563c3c9e48257968e6cac7d84e4dab2f
leoCardosoDev <leocardosodev@gmail.com>
2023-12-11 17:34:50 -0300

    test: ensure EmailValidation return error if email is invalid

--- test/validation/validators/email_field_validaton_test.dart
+++ test/validation/validators/email_field_validaton_test.dart
@@ -14,4 +14,8 @@ void main() {
  test('Should return null if email is valid', () {
   expect(sut.validate('any_email@mail.com'), null);
  });
+
+ test('Should return error if email is invalid', () {
+  expect(sut.validate('any_email@'), 'E-mail inválido');
+ });
 }
