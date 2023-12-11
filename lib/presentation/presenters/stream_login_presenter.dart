dcef3da08ec1005512f8447206e0401eda616c80
leoCardosoDev <leocardosodev@gmail.com>
2023-12-11 14:36:15 -0300

    feat: ensure StreamLoginPresenter calls Authentication with correct values

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import '../../domain/usecases/usecases.dart';
 import '../protocols/protocols.dart';
 
 class LoginState {
@@ -12,12 +13,13 @@ class LoginState {
 
 class StreamLoginPresenter {
   final Validation validation;
+  final Authenticathion authentication;
   final _controller = StreamController<LoginState>.broadcast();
   final _state = LoginState();
   Stream<String?> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
   Stream<String?> get passwordErrorStream => _controller.stream.map((state) => state.passwordError).distinct();
   Stream<bool?> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
-  StreamLoginPresenter({required this.validation});
+  StreamLoginPresenter({required this.validation, required this.authentication});
 
   void _update() => _controller.add(_state);
 
@@ -31,5 +33,9 @@ class StreamLoginPresenter {
     _state.password = password;
     _state.passwordError = validation.validate(field: 'password', value: password);
     _update();
+  }
+
+  Future<void> auth() async {
+    await authentication.auth(AuthenticationParams(email: _state.email!, secret: _state.password!));
   }
 }
