8fa48f0e521eda3db04c754d56ae4e4a6749a629
leoCardosoDev <leocardosodev@gmail.com>
2023-12-11 14:35:30 -0300

    refactor: add Equatable and return null for AccountEntity

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,11 +1,16 @@
+import 'package:equatable/equatable.dart';
+
 import '../entities/entities.dart';
 
 abstract class Authenticathion {
-  Future<AccountEntity> auth(AuthenticationParams params);
+  Future<AccountEntity>? auth(AuthenticationParams params);
 }
 
-class AuthenticationParams {
+class AuthenticationParams extends Equatable {
   final String email;
   final String secret;
-  AuthenticationParams({required this.email, required this.secret});
+  const AuthenticationParams({required this.email, required this.secret});
+  
+  @override
+  List<Object?> get props => [email, secret];
 }
