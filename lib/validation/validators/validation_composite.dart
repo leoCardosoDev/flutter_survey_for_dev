5b81c7ea9716be7a6da72cb42bbeb8547a308797
leoCardosoDev <leocardosodev@gmail.com>
2023-12-17 06:39:37 -0300

    feat: ensure ValidationComposite returns first error of the correct field

--- lib/validation/validators/validation_composite.dart
+++ lib/validation/validators/validation_composite.dart
@@ -8,7 +8,7 @@ class ValidationComposite implements Validation {
   @override
   String? validate({required String field, required String value}) {
     String? error;
-    for (final validation in validations) {
+    for (final validation in validations.where((v) => v.field == field)) {
       error = validation.validate(value);
       if (error?.isNotEmpty == true) {
         return error;
