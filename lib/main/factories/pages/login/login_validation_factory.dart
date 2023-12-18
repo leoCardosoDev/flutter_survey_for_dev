e2de6d798e21ab7b8d36469d1388885b87e9ba92
leoCardosoDev <leocardosodev@gmail.com>
2023-12-18 06:16:18 -0300

    feat: ensure LoginValidationsFactory returns correct validations

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,9 +1,13 @@
 import '../../../../presentation/protocols/protocols.dart';
+import '../../../../validation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
 
-Validation makeLoginValidation() => 
-  ValidationComposite([
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password')
-  ]);
+Validation makeLoginValidation() => ValidationComposite(makeLoginValidations());
+
+List<FieldValidation> makeLoginValidations() {
+  return [
+    const RequiredFieldValidation('email'),
+    const EmailValidation('email'),
+    const RequiredFieldValidation('password')
+  ];
+}
