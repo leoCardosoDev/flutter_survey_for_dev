e11b3c3503d6504bc3359efbc1fa3ce9f0a75b15
leoCardosoDev <leocardosodev@gmail.com>
2023-12-27 07:09:19 -0300

    feat: add ValidatorBuild

--- lib/main/builders/validation_builder.dart
+++ lib/main/builders/validation_builder.dart
@@ -3,12 +3,13 @@ import '../../validation/validators/validators.dart';
 
 class ValidationBuilder {
   static ValidationBuilder? _instance;
-  late String fieldName;
+  String fieldName;
   List<FieldValidation> validations = [];
 
+  ValidationBuilder._(this.fieldName);
+
   static ValidationBuilder field(String fieldName) {
-    _instance ??= ValidationBuilder();
-    _instance!.fieldName = fieldName;
+    _instance = ValidationBuilder._(fieldName);
     return _instance!;
   }
 
@@ -18,7 +19,9 @@ class ValidationBuilder {
   }
   
   ValidationBuilder email() {
-    validations.add(RequiredFieldValidation(fieldName));
+    validations.add(EmailValidation(fieldName));
     return this;
   }
+
+  List<FieldValidation> build() => validations;
 }
