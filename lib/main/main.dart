7fb1f00166763ec85881a438bd767d8df6c33464
leoCardosoDev <leocardosodev@gmail.com>
2024-01-04 18:38:11 -0300

    chore: add Provider debugCheckInvalidValueType

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 import '../ui/components/components.dart';
 import './factories/factories.dart';
 
 void main() {
+  Provider.debugCheckInvalidValueType = null;
   runApp(const App());
 }
 
