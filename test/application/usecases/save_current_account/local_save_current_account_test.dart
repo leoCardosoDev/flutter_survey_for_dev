170c2e4069e865918e81078b0cf322e17a87a6c6
leoCardosoDev <leocardosodev@gmail.com>
2024-01-10 17:24:10 -0300

    refactor: move SaveSecureCacheStorage mock to a helper method

--- test/application/usecases/save_current_account/local_save_current_account_test.dart
+++ test/application/usecases/save_current_account/local_save_current_account_test.dart
@@ -10,20 +10,34 @@ import 'package:survey_for_dev/application/usecases/usecases.dart';
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
 void main() {
+  late SaveSecureCacheStorageSpy saveSecureCacheStorage;
+  late LocalSaveCurrentAccount sut;
+  late AccountEntity account;
+
+  When mockWhen () => when(() => saveSecureCacheStorage.saveSecure(key: any(named: 'key'), value: any(named: 'value')));
+
+  void mockSuccess () {
+    mockWhen().thenAnswer((_) async => Future.value());
+  }
+
+  void mockException() {
+    mockWhen().thenThrow(Exception());
+  }
+
+  setUp(() {
+    saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    account = AccountEntity(faker.guid.guid());
+    mockSuccess();
+  });
+
  test('should call SaveSecureCacheStorage with correct values', () async {
-  final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-  final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-  final account = AccountEntity(faker.guid.guid());
-  when(() => saveSecureCacheStorage.saveSecure(key: any(named: 'key'), value: any(named: 'value'))).thenAnswer((_) async => Future.value());
   await sut.save(account);
   verify(() => saveSecureCacheStorage.saveSecure(key: 'token', value: account.token)).called(1);
  });
 
   test('should throw UnexpectedError if SaveSecureCacheStorage throws', () async {
-  final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-  final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-  final account = AccountEntity(faker.guid.guid());
-  when(() => saveSecureCacheStorage.saveSecure(key: any(named: 'key'), value: any(named: 'value'))).thenThrow(Exception());
+  mockException();
   final future = sut.save(account);
   expect(future, throwsA(DomainError.unexpected));
  });
