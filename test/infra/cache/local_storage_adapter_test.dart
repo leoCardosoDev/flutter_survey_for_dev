79ea2186d0530c7164eaefd4a3804c05ea5c2c50
leoCardosoDev <leocardosodev@gmail.com>
2024-01-11 17:55:10 -0300

    refactor: move sut creation to a setup method

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -8,12 +8,26 @@ import 'package:survey_for_dev/infra/cache/cache.dart';
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
 void main() {
+  late LocalStorageAdapter sut;
+  late FlutterSecureStorageSpy secureStorage;
+  late String key;
+  late String value;
+
+  When mockSecureStorage () => when(() => secureStorage.write(key: any(named: 'key'), value: any(named: 'value')));
+
+  void mockSuccess() {
+    mockSecureStorage().thenAnswer((_) async => Future.value());
+  }
+
+  setUp(() {
+    secureStorage = FlutterSecureStorageSpy();
+    sut = LocalStorageAdapter(secureStorage: secureStorage);
+    key = faker.lorem.word();
+    value = faker.guid.guid();
+    mockSuccess();
+  });
+
  test('Should call save secure with correct values', () async {
-  final secureStorage = FlutterSecureStorageSpy();
-  final sut = LocalStorageAdapter(secureStorage: secureStorage);
-  final key = faker.lorem.word();
-  final value = faker.guid.guid();
-  when(() => secureStorage.write(key: any(named: 'key'), value: any(named: 'value'))).thenAnswer((_) async => Future.value());
   await sut.saveSecure(key: key, value: value);
   verify(() => secureStorage.write(key: key, value: value));
  });
